6255369b2c260ca8a300532ee0228af423c06cb1
IsracardTeam <33055665+IsracardTeam@users.noreply.github.com>
2020-08-27 20:23:56 +0300

    Added condition to focus on a node only if screen reader is enabled

--- index.js
+++ index.js
@@ -7,18 +7,29 @@ const FOCUS_ON_VIEW = 8;
 
 const announceForAccessibility = Platform.OS === 'android' ? RNAccessibility.announce : AccessibilityInfo.announceForAccessibility;
 
+
 module.exports = {
     announceForAccessibility,
     focusOnView(ref) {
+
         if(!ref) {
             console.warn("ref is null")
             return
         }
-        const reactTag = findNodeHandle(ref)
+
+        try{
+            AccessibilityInfo.isScreenReaderEnabled().then(isEnabled => {
+             if(isEnabled){
+                const reactTag = findNodeHandle(ref)
  
-        Platform.OS === 'android' ? UIManager.sendAccessibilityEvent(
-            reactTag,
-            FOCUS_ON_VIEW
-        ) : AccessibilityInfo.setAccessibilityFocus(reactTag)
-    }
+                Platform.OS === 'android' ? UIManager.sendAccessibilityEvent(
+                    reactTag,
+                    FOCUS_ON_VIEW
+                ) : AccessibilityInfo.setAccessibilityFocus(reactTag)
+             }
+            });
+        }catch(e){
+            console.log(e)
+        }
+    }   
 }
